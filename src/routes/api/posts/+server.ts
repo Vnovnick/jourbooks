13bc863eecaf4888c331927ec3fc358a7b8c88af
Vnovnick <safonov.nick10@gmail.com>
2023-10-02 21:01:09 -0700

    post still not working

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -16,13 +16,13 @@ export const GET = async ({ request }) => {
 
 export const POST = async ({ request }) => {
   const body = await request.json();
-  const authHeader = request.headers.get("Authorization");
+  // const authHeader = request.headers.get("Authorization");
 
-  if (authHeader != "myheader") {
-    return new Response(JSON.stringify({ message: "invalid auth" }), {
-      status: 401,
-    });
-  }
+  // if (authHeader != "myheader") {
+  //   return new Response(JSON.stringify({ message: "invalid auth" }), {
+  //     status: 401,
+  //   });
+  // }
   // const res = await fetch("http://localhost:3000/", {
   //   method: "POST",
   //   mode: "cors", // this cannot be 'no-cors'
@@ -32,7 +32,7 @@ export const POST = async ({ request }) => {
   //   },
   //   body: JSON.stringify(body),
   // }).then((res) => res.json());
-  const res = await axios.post("http://localhost:3000/", body);
+  const res = await axios.post("http://localhost:3000/", JSON.stringify(body));
   console.log(res.data);
 
   return new Response(JSON.stringify(res.data), { status: 201 });
