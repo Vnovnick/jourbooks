512412163da8002e39ff0970bf86318456976dbd
Vnovnick <safonov.nick10@gmail.com>
2023-10-03 18:27:02 -0700

    clean post after backend fix

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -16,24 +16,14 @@ export const GET = async ({ request }) => {
 
 export const POST = async ({ request }) => {
   const body = await request.json();
-  // const authHeader = request.headers.get("Authorization");
+  const authHeader = request.headers.get("Authorization");
 
-  // if (authHeader != "myheader") {
-  //   return new Response(JSON.stringify({ message: "invalid auth" }), {
-  //     status: 401,
-  //   });
-  // }
-  // const res = await fetch("http://localhost:3000/", {
-  //   method: "POST",
-  //   mode: "cors", // this cannot be 'no-cors'
-  //   headers: {
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(body),
-  // }).then((res) => res.json());
-  const res = await axios.post("http://localhost:3000/", JSON.stringify(body));
-  console.log(res.data);
+  if (authHeader != "myheader") {
+    return new Response(JSON.stringify({ message: "invalid auth" }), {
+      status: 401,
+    });
+  }
+  const res = await axios.post("http://localhost:3000/", body);
 
   return new Response(JSON.stringify(res.data), { status: 201 });
 };
