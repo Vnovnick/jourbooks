68197e2ccca08e1b464d1944700341e821a616c3
Vnovnick <safonov.nick10@gmail.com>
2023-10-04 21:57:13 -0700

    extract Register form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
+  import Register from "./Register.svelte";
 
   export let form;
-  let userName: string;
   let userEmail: string;
   let userPassword: string;
-  let confirmPassword: string;
   let isLoginView = true;
   const loginInputs = "w-[300px] rounded-md h-10 pl-2";
   const formMessages = "w-[450px] m-auto rounded-md p-1 text-left";
@@ -35,62 +33,7 @@
       </div>
     </form>
   {:else}
-    <form
-      class="w-[550px] flex flex-col gap-y-4 my-auto"
-      method="post"
-      action="?/register"
-      use:enhance
-    >
-      <p class="text-xl font-semibold text-center">Create a New Account</p>
-      <div class="w-full justify-between flex">
-        <p>User Name:</p>
-        <input
-          class={loginInputs}
-          type="text"
-          bind:value={userName}
-          name="username"
-          required
-        />
-      </div>
-      <div class="w-full justify-between flex">
-        <p>Email:</p>
-        <input
-          class={loginInputs}
-          type="text"
-          bind:value={userEmail}
-          name="email"
-          required
-        />
-      </div>
-      <div class="w-full justify-between flex">
-        <p>Password:</p>
-        <input
-          class={loginInputs}
-          type="password"
-          bind:value={userPassword}
-          name="password"
-          required
-        />
-      </div>
-      <div class="w-full justify-between flex">
-        <p>Confirm Password:</p>
-        <input
-          class={loginInputs}
-          type="password"
-          bind:value={confirmPassword}
-          name="confirmPassword"
-          required
-        />
-      </div>
-      <div class="flex mx-auto gap-5 text-sm">
-        <button
-          class={buttonStyling}
-          type="button"
-          on:click={() => (isLoginView = true)}>Cancel</button
-        >
-        <button class={buttonStyling} type="submit">Create Account</button>
-      </div>
-    </form>
+    <Register {loginInputs} {buttonStyling} bind:isLoginView />
   {/if}
   <div class="flex w-[550px]">
     {#if form?.success}
---
+++ src/routes/Register.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+
+  export let loginInputs: string;
+  export let buttonStyling: string;
+  export let isLoginView: boolean;
+  let newUserName: string;
+  let newUserEmail: string;
+  let newUserPassword: string;
+  let confirmPassword: string;
+</script>
+
+<form
+  class="w-[550px] flex flex-col gap-y-4 my-auto"
+  method="post"
+  action="?/register"
+  use:enhance
+>
+  <p class="text-xl font-semibold text-center">Create a New Account</p>
+  <div class="w-full justify-between flex">
+    <p>User Name:</p>
+    <input
+      class={loginInputs}
+      type="text"
+      bind:value={newUserName}
+      name="username"
+      required
+    />
+  </div>
+  <div class="w-full justify-between flex">
+    <p>Email:</p>
+    <input
+      class={loginInputs}
+      type="text"
+      bind:value={newUserEmail}
+      name="email"
+      required
+    />
+  </div>
+  <div class="w-full justify-between flex">
+    <p>Password:</p>
+    <input
+      class={loginInputs}
+      type="password"
+      bind:value={newUserPassword}
+      name="password"
+      required
+    />
+  </div>
+  <div class="w-full justify-between flex">
+    <p>Confirm Password:</p>
+    <input
+      class={loginInputs}
+      type="password"
+      bind:value={confirmPassword}
+      name="confirmPassword"
+      required
+    />
+  </div>
+  <div class="flex mx-auto gap-5 text-sm">
+    <button
+      class={buttonStyling}
+      type="button"
+      on:click={() => (isLoginView = true)}>Cancel</button
+    >
+    <button class={buttonStyling} type="submit">Create Account</button>
+  </div>
+</form>
