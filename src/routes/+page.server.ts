d6f61a2112cbb61cc3d1ad4f5fc3816d0a1f664e
Vnovnick <safonov.nick10@gmail.com>
2023-10-06 19:02:33 -0700

    display correct error message from register request

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const actions = {
   register: async ({ request }) => {
+    let formError: string = "Error Creating New User";
     const formData = await request.formData();
     const username = formData.get("username") as string;
     const email = formData.get("email") as string;
@@ -14,24 +15,25 @@ export const actions = {
       password,
       confirmPassword,
     };
-    if (password !== confirmPassword) {
-      return fail(400, {
-        error: "Passwords do not match",
-      });
-    } else if (password.length < 6) {
+    if (password.length < 6) {
       return fail(400, {
         error: "Password must be at least 6 characters",
+      });
+    } else if (password !== confirmPassword) {
+      return fail(400, {
+        error: "Passwords do not match",
       });
     }
 
     const res = await axios
-      .post("http://localhost:3000", body)
+      .post("http://localhost:3000/register", body)
       .catch((error) => {
         console.log(error);
-        return fail(500, { error });
+        formError = error.response.data.message;
+        return fail(500, { error: formError });
       });
 
     if (res.status === 201) return { success: true };
-    return fail(500, { error: "Patch Failed" });
+    return fail(500, { error: formError });
   },
 };
