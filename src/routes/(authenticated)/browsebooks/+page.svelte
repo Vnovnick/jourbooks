5221e001ec1098615bcebd30d8249b12e353fcaa
Vnovnick <safonov.nick10@gmail.com>
2023-10-20 17:43:22 -0700

    fix whitespace issue; change search into form handled locally

--- src/routes/(authenticated)/browsebooks/+page.svelte
+++ src/routes/(authenticated)/browsebooks/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+  import { enhance } from "$app/forms";
   import { searchPlaceholders } from "$lib/browsebooksDefinitions";
 
   let bookSearch: string;
   const placeholder =
     searchPlaceholders[Math.floor(Math.random() * searchPlaceholders.length)];
 
-  // need to remove whitespace from each individual word before joining
-  const handleBookSearch = () => {
-    const convertedSearch = bookSearch.trim().split(" ");
-    // .map((s) => s.trim())
-    // .join("+");
+  const handleBookSearch = (e: SubmitEvent) => {
+    e.preventDefault();
+    const convertedSearch = bookSearch
+      .trim()
+      .split(" ")
+      .filter((s) => !!s.length)
+      .join("+");
     console.log(convertedSearch);
   };
 </script>
@@ -23,7 +26,10 @@
     </form>
   </nav>
   <p class="text-5xl text-center mt-10">Browse Books</p>
-  <div class="flex gap-x-2 items-center mt-10 justify-center">
+  <form
+    class="flex gap-x-2 items-center mt-10 justify-center"
+    on:submit={handleBookSearch}
+  >
     <input
       class="w-[600px] h-9 pl-2 border border-black"
       name="book-search"
@@ -31,9 +37,9 @@
       bind:value={bookSearch}
     />
     <button
-      type="button"
+      type="submit"
       class="p-1.5 bg-black text-green-50 rounded-sm hover:bg-black h-fit"
-      on:click={handleBookSearch}>Search</button
+      >Search</button
     >
-  </div>
+  </form>
 </div>
