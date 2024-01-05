0bb22bf350a75bd5c39759e654eb40f9ce5bb07c
Vnovnick <safonov.nick10@gmail.com>
2024-01-04 18:51:33 -0800

    fix query parameters and parameter page history tracking; add loading state for search

--- src/routes/(authenticated)/browsebooks/+page.svelte
+++ src/routes/(authenticated)/browsebooks/+page.svelte
@@ -1,33 +1,44 @@
 <script lang="ts">
   export let data;
+  import { page } from "$app/stores";
   import { searchPlaceholders } from "$lib/browsebooksDefinitions";
   import { primaryActionButton } from "$lib/standardStyles";
   import axios from "axios";
   import BookSearchEntry from "./BookSearchEntry.svelte";
   import type { Book } from "$lib/typesAndInterfaces";
   import { expressServerURL } from "$lib/endpointAssets";
+  import { goto } from "$app/navigation";
+  import Jellyfish from "svelte-loading-spinners/Jellyfish.svelte";
 
+  let isSearching = false;
+  let form: any;
   let bookSearch: string;
+  let convertedSearch = "";
   let searchResponse: any[] = [];
   let numResults: number;
   let bookSearchError: string | null = null;
   const placeholder =
     searchPlaceholders[Math.floor(Math.random() * searchPlaceholders.length)];
 
+  const hasQueryParams = $page.url.searchParams.has("q");
+
   const handleBookSearch = async (e: SubmitEvent) => {
     e.preventDefault();
+    isSearching = true;
     searchResponse = [];
-    const convertedSearch = bookSearch
-      .trim()
-      .split(" ")
-      .filter((s) => !!s.length)
-      .join("+");
-
-    // TODO needs looking into - goal is to have query accessible and part of route so that the page can then be re-loaded with the search
-    // const searchParams = new URLSearchParams(window.location.search);
-    // searchParams.set("q", convertedSearch);
-    // const newpath = window.location.pathname + "?" + searchParams.toString();
-    // history.pushState(null, "", newpath);
+    const searchQuery = $page.url.searchParams.get("q");
+    if (hasQueryParams && searchQuery && !bookSearch) {
+      convertedSearch = searchQuery;
+    } else {
+      convertedSearch = bookSearch
+        .trim()
+        .split(" ")
+        .filter((s) => !!s.length)
+        .join("+");
+      const query = new URLSearchParams($page.url.searchParams.toString());
+      query.set("q", convertedSearch);
+      goto(`?${query.toString()}`);
+    }
 
     if (convertedSearch.length > 0) {
       const res = await axios.get(
@@ -39,6 +50,7 @@
         const shelvedBooks = await axios.get(
           `${expressServerURL}/v1/book/shelved/all/${data.id}`
         );
+
         searchResponse = res.data.docs.map((doc: any) => {
           const docOlid = doc.key.slice(7);
           const matchingBook = shelvedBooks.data.find(
@@ -55,7 +67,10 @@
         console.log(bookSearchError);
       }
     }
+    isSearching = false;
   };
+
+  $: if ($page.url.searchParams.get("q") && form) form.requestSubmit();
 </script>
 
 <div class="bg-green-50 flex flex-col overflow-auto">
@@ -63,6 +78,7 @@
   <form
     class="flex gap-x-2 mt-10 mb-5 justify-center"
     on:submit={handleBookSearch}
+    bind:this={form}
   >
     <input
       class="w-[600px] h-9 pl-2 border border-black"
@@ -86,6 +102,11 @@
       }}>Clear Results</button
     >
   </div>
+  {#if isSearching}
+    <div class="mt-24 mx-auto">
+      <Jellyfish size="100" color="#6ee7b7" unit="px" duration="3s" />
+    </div>
+  {/if}
   {#if searchResponse.length > 0}
     <div class="min-h overflow-auto px-5 flex flex-col gap-y-3">
       {#each searchResponse as book}
